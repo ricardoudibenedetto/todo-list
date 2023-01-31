9ebb8030e0b5177bae13047be2c3ee1fe3c89e16
rd_i01880@redlink.com.ar <r.dibenedetto@redlink.com.ar>
2023-01-31 18:25:46 -0300

    hook todo unico

---
+++ src/hooks/useTodo.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { Todo } from "../models/todo.api.models"
+
+export const useTodo = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const todoAdd = (description: string) => {
+        if (description.trim().length < 1) return;
+        if (todos.some((todo: Todo) => todo.description == description)) return;
+        setTodos([...todos, { description, isComplete: false }])
+    }
+
+    const todoComplete = (description: string) => {
+        setTodos([...todos.map((todo: Todo) => todo.description == description ? { ...todo, isComplete: true } : todo)])
+    }
+    
+    const todoIncomplete = (description: string) => {
+        setTodos([...todos.map((todo: Todo) => todo.description == description ? { ...todo, isComplete: false } : todo)])
+    }
+
+    return {
+        todos,
+        todoAdd,
+        todoComplete,
+        todoIncomplete
+    }
+}
